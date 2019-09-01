95bcad79a90c40d6ce584f0d2f5fc9c66ee8ea48
lil988 <53288044+lil988@users.noreply.github.com>
2019-09-01 07:55:07 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+
+<!--
+File Name: index.html
+Date: 07/29/2019
+Programmer: Juana Maquin
+-->
+
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<meta name="author" content="Juana Maquin">
+
+  <!-- <base target="_blank">   -->
+	 <!-- open all links not marked "_self" in a new tab -->
+	<title>Ace in the Hole Multisport Events</title>
+<!-- open all links not marked "_self" in a new tab -->
+	<link href="template/css/bootstrap.min.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="template/css/reset.css" rel="stylesheet" type="text/css">
+	<link href="template/css/grid.css" rel="stylesheet" type="text/css">
+	<link href="template/css/style.css" rel="stylesheet" type="text/css">
+	<link href="template/css/navigation.css" rel="stylesheet" type="text/css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
+
+
+
+
+
+
+</head>
+
+<body>
+
+
+
+  <?php
+
+      include 'template/includes/header.php';
+      include 'template/includes/content.php';
+      include 'template/includes/footer.php';
+
+
+   ?>
+
+
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="template/js/bootstrap.min.js"></script>
+</body>
+
+</html>
